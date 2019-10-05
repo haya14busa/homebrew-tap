e535a7686dab4cf4631dc4892e97b5be304837e3
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-06 02:46:04 +0900

    Brew formula update for ghglob version v2.0.0

--- Formula/ghglob.rb
+++ Formula/ghglob.rb
@@ -2,16 +2,16 @@
 class Ghglob < Formula
   desc "ghglob is glob, or more like pattern matcher based on GitHub Actions's Filter pattern spec."
   homepage "https://github.com/haya14busa/ghglob"
-  version "1.0.1"
+  version "2.0.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/haya14busa/ghglob/releases/download/v1.0.1/ghglob_1.0.1_Darwin_x86_64.tar.gz"
-    sha256 "79dbf4a149db4c566c641bec1086ab3422cee5a0cd53f43e5aaf4267da435ccd"
+    url "https://github.com/haya14busa/ghglob/releases/download/v2.0.0/ghglob_2.0.0_Darwin_x86_64.tar.gz"
+    sha256 "1d4de55b8a64043a5971ea98a0f282745f6b8e80395c2bbfe0777fd18237b5e7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/haya14busa/ghglob/releases/download/v1.0.1/ghglob_1.0.1_Linux_x86_64.tar.gz"
-      sha256 "8b1b73349c20e28f207ea17b979e95b8489d3e3333793c474f1700c68637caf8"
+      url "https://github.com/haya14busa/ghglob/releases/download/v2.0.0/ghglob_2.0.0_Linux_x86_64.tar.gz"
+      sha256 "7883623767ae3a079a18834abe626e144fe848c7a5529b83a3f793d1c7eafe2b"
     end
   end
 
